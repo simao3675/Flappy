f77c9a4a3e19c8410262cbecd719898b085824b7
simao762 <s10635105@gm.cyut.edu.tw>
2019-07-23 00:15:47 +0800

    complete

---
+++ Flappy/Assets/ScreenControl.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ScreenControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //開始方法:遊戲開始時執行一次
+    void Start()
+    {
+	//設定螢幕(寬，高，是否全螢幕)
+        Screen.SetResolution(576,1024,false);
+    }
+}
